a34ad36a49c5c079e26c7ddbfdbe9b25a89742e4
Piyush Acharya <acharyapiyush1@outlook.com>
2023-12-26 16:12:37 -0800

    Initial Commit

---
+++ ClearCapture/ClearCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearCaptureApp.swift
+//  ClearCapture
+//
+//  Created by Piyush Acharya on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ClearCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
